78ae579b49c44adb0684e2bf8d8987971170b74f
shantalanarayan <mail2shantala@gmail.com>
2020-03-12 12:49:47 -0400

    Added ability to navigate to create quiz component Added navigation link to create Quiz in MyTopic Also, added react-bootstrap UI for listing topics

--- src/components/Quiz/MyTopic/MyTopic.js
+++ src/components/Quiz/MyTopic/MyTopic.js
@@ -1,24 +1,36 @@
 import React, { Fragment, Component } from 'react'
 import { withRouter } from 'react-router-dom'
-import Card from 'react-bootstrap/Card'
+import ListGroup from 'react-bootstrap/ListGroup'
+import Button from 'react-bootstrap/Button'
+import Jumbotron from 'react-bootstrap/Jumbotron'
 
 class MyTopic extends Component {
   constructor () {
     super()
     this.state = {
-      topic: {
-        title: '',
-        questions: []
-      }
+      topic: []
     }
   }
 
   render () {
     return (
       <Fragment>
-        <Card className="m-5">
-          <Card.Body>My topics - {this.props.user.email}</Card.Body>
-        </Card>
+        <Jumbotron className="mt-5">
+          <div className="row">
+            <div className="col-12">
+              <Button className="btn btn-primary float-right m-3" href="#create-quiz">+ Create a new Topic</Button>
+            </div>
+          </div>
+          <div className="col-12">
+            <ListGroup>
+              <ListGroup.Item>Cras justo odio</ListGroup.Item>
+              <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+              <ListGroup.Item>Morbi leo risus</ListGroup.Item>
+              <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
+              <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+            </ListGroup>
+          </div>
+        </Jumbotron>
       </Fragment>
     )
   }
