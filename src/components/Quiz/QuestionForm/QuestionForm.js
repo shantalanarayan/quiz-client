8cb1d56cd5964e55c398125e79a952c1d44a5fa4
shantalanarayan <mail2shantala@gmail.com>
2020-03-12 22:30:51 -0400

    UI changes for adding questions when creating a new quiz Added a new form with validation in a modal to capture a set of question and answers Added accordion to display questions list in the topic

---
+++ src/components/Quiz/QuestionForm/QuestionForm.js
@@ -0,0 +1,83 @@
+import React from 'react'
+import Form from 'react-bootstrap/Form'
+
+const QuestionForm = ({ topic, validated, handleSave, handleChange }) => {
+  return (
+    <div>
+      <Form noValidate validated={validated} className="mt-5" onSubmit={handleSave}>
+        <Form.Group>
+          <Form.Label>Question</Form.Label>
+          <Form.Control
+            required
+            type="text"
+            name="questions"
+            value={topic.questions}
+            placeholder="Enter a question"
+            onChange={handleChange}
+          />
+          <Form.Control.Feedback type="invalid">
+            Please enter a question.
+          </Form.Control.Feedback>
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Correct Answer</Form.Label>
+          <Form.Control
+            required
+            type="text"
+            name="correct_ans"
+            value={topic.correct_ans}
+            placeholder="Enter correct answer here"
+            onChange={handleChange}
+          />
+          <Form.Control.Feedback type="invalid">
+            Please enter an answer for correct answer.
+          </Form.Control.Feedback>
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Incorrect Answer 1</Form.Label>
+          <Form.Control
+            required
+            type="text"
+            name="incorrect_ans1"
+            value={topic.incorrect_ans1}
+            placeholder="Enter 1st incorrect answer here"
+            onChange={handleChange}
+          />
+          <Form.Control.Feedback type="invalid">
+            Please enter an answer for 1st incorrect answer.
+          </Form.Control.Feedback>
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Incorrect Answer 2</Form.Label>
+          <Form.Control
+            required
+            type="text"
+            name="incorrect_ans2"
+            value={topic.incorrect_ans2}
+            placeholder="Enter 2nd incorrect answer here"
+            onChange={handleChange}
+          />
+          <Form.Control.Feedback type="invalid">
+            Please enter an answer for 2nd incorrect answer.
+          </Form.Control.Feedback>
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Incorrect Answer 3</Form.Label>
+          <Form.Control
+            required
+            type="text"
+            name="incorrect_ans3"
+            value={topic.incorrect_ans3}
+            placeholder="Enter 3rd incorrect answer here"
+            onChange={handleChange}
+          />
+          <Form.Control.Feedback type="invalid">
+            Please enter an answer for 3rd incorrect answer.
+          </Form.Control.Feedback>
+        </Form.Group>
+      </Form>
+    </div>
+  )
+}
+
+export default QuestionForm
---
+++ src/components/Quiz/QuestionModal/QuestionModal.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button'
+import Modal from 'react-bootstrap/Modal'
+import QuestionForm from '../QuestionForm/QuestionForm'
+
+const QuestionModal = ({ show, validated, handleShow, handleClose, topic, handleSave, handleChange }) => {
+  return (
+    <div>
+      <Button variant="primary" onClick={handleShow}>
+        + Add a new question
+      </Button>
+
+      <Modal show={show} onHide={handleClose} animation={false}>
+        <Modal.Header closeButton>
+          <Modal.Title>Question Form</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <QuestionForm
+            topic={topic}
+            validated={validated}
+            handleChange={handleChange}
+            handleSave={handleSave}
+          />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleSave}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  )
+}
+
+export default QuestionModal
