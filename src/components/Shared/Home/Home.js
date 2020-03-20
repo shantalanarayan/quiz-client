02acd642e85cbaff1b95d29fab99c1a5373f8bfb
shantalanarayan <mail2shantala@gmail.com>
2020-03-20 13:44:41 -0400

    Restyled fonts and welcome message Updated navbar to include user profile on login Updated fonts on navbar and welcome text Updated welcome text Added some styling to cards

--- src/components/Shared/Home/Home.js
+++ src/components/Shared/Home/Home.js
@@ -1,15 +1,28 @@
 import React, { Fragment } from 'react'
-// import Card from 'react-bootstrap/Card'
+import Card from 'react-bootstrap/Card'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSmile } from '@fortawesome/free-solid-svg-icons'
 
-const genericWelcome = 'Welcome to Quiz-up'
+const genericWelcome = (
+  <div className="welcome-text">
+    <h1>Welcome to Quiz-Up!</h1>
+    To create or manage your quiz bank please <a href="#sign-in">sign-in</a> first.
+  </div>
+)
 
-const userWelcome = ' welcome to Quiz-up. Please head to admin to add or edit topics.'
+const userWelcome = (user) => (
+  <div className="welcome-text">
+    <h1>Welcome { user.email }!</h1>
+    To create or manage your quiz bank please head to <a href="#my-topics">My Topics</a> to manage your topics
+  </div>
+)
 
 const Home = ({ user }) => (
   <Fragment>
-    <div className="m-5 welcome-text">
-      { user ? user.email + userWelcome : genericWelcome }
-    </div>
+    <Card className="m-5 pl-5">
+      <FontAwesomeIcon icon={faSmile} size="6x" className="m-4"/>
+      { user ? userWelcome(user) : genericWelcome }
+    </Card>
   </Fragment>
 )
 
